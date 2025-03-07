32f9e79a3135da33d6a87c76acd8363297c4dc4b
JosefaOgalde <105354294+JosefaOgalde@users.noreply.github.com>
2025-03-07 14:34:23 -0300

    Update Interfaz.java

--- Interfaz.java
+++ Interfaz.java
@@ -1,41 +1,43 @@
 import java.util.Scanner;
 
-public class Intefaz {
+public class Interfaz {
     public static void main(String[] args) {
+
+        System.out.println("Por favor introduzca su número de identificación");
+        Scanner scanner = new Scanner(System.in);
+        String identificador = scanner.nextLine();
+        System.out.println("Por favor introduzca su contraseña");
+        String contraseña = scanner.nextLine();
+        CajeroAutomatico cajero = new CajeroAutomatico(identificador, contraseña);
+
         int opcionSeleccionada;
         do {
-            System.out.println("Elija una de las siguientes opciones");
+            System.out.println("Elija una de las siguientes opciones:");
             System.out.println("   Escriba 1 para consultar su saldo");
             System.out.println("   Escriba 2 para ingresar dinero");
             System.out.println("   Escriba 3 para sacar dinero");
             System.out.println("   Escriba 4 para consultar sus últimos movimientos");
-            System.out.println("   Para salir escriba cualquier otro número ");
+            System.out.println("   Para salir escriba cualquier otro número");
 
-            Scanner scanner = new Scanner(System.in);
+            opcionSeleccionada = scanner.nextInt();
 
-                opcionSeleccionada = scanner.nextInt();
-
-
-            switch (opcionSeleccionada){
+            switch (opcionSeleccionada) {
                 case 1:
-                    System.out.println("La opción que usted ha elegido es consultar su saldo");
+                    cajero.mostrarSaldo();
                     break;
                 case 2:
-                    System.out.println("La opción que usted ha elegido es ingresar dinero");
+                    cajero.ingresarDinero();
                     break;
                 case 3:
-                    System.out.println("La opción que usted ha elegido es sacar dinero");
+                    cajero.sacarDinero();
                     break;
                 case 4:
-                    System.out.println("La opción que usted ha elegido es consultar sus últimos movimientos");
+                    cajero.consultarUltimosMovimientos();
                     break;
                 default:
-                    System.out.println("Muchas gracias por usar nuestros servicios");
-
+                    cajero.salir();
             }
-
-        }  while (opcionSeleccionada == 1 || opcionSeleccionada == 2 || opcionSeleccionada == 3 || opcionSeleccionada == 4);
-
+        } while (opcionSeleccionada == 1 || opcionSeleccionada == 2 || opcionSeleccionada == 3 || opcionSeleccionada == 4);
 
     }
 }
